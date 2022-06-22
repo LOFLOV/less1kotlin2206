f5f3ad8de6229d8c658124c1319235b62325cb88
JB <jb>
2022-06-22 15:10:40 +0200

    Created data class. Properties added to text view.

---
+++ app/src/main/java/com/android/less1kotlin2206/DataTest.kt
@@ -0,0 +1,3 @@
+package com.android.less1kotlin2206
+
+data class DataTest(val text: String, val value: Int)
--- app/src/main/java/com/android/less1kotlin2206/MainActivity.kt
+++ app/src/main/java/com/android/less1kotlin2206/MainActivity.kt
@@ -2,6 +2,7 @@ package com.android.less1kotlin2206
 
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.AppCompatButton
@@ -15,5 +16,12 @@ class MainActivity : AppCompatActivity() {
         btn.setOnClickListener{
             Toast.makeText(baseContext, "clicked", Toast.LENGTH_SHORT).show()
         }
+
+        val mainTextView = findViewById<TextView>(R.id.main_text_view)
+        val secondTextView = findViewById<TextView>(R.id.second_text_view)
+        val dataClass = DataTest("1", 2)
+
+        mainTextView.text = dataClass.text
+        secondTextView.text = dataClass.value.toString()
     }
 }
