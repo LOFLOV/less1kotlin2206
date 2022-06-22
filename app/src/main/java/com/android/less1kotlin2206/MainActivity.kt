d1f15ae33b93ca0c08e077ab21b65abf1274dea5
JB <jb>
2022-06-22 11:04:54 +0200

    Added button with clickListener to MaimActivity.

--- app/src/main/java/com/android/less1kotlin2206/MainActivity.kt
+++ app/src/main/java/com/android/less1kotlin2206/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.android.less1kotlin2206
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.AppCompatButton
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btn = findViewById<AppCompatButton>(R.id.btn_action)
+        btn.setOnClickListener{
+            Toast.makeText(baseContext, "clicked", Toast.LENGTH_SHORT).show()
+        }
     }
 }
