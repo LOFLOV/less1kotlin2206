24e7bbf9061770a1ea9115d01ac136bde87719f8
JB <jb>
2022-06-22 10:39:54 +0200

    Initial commit

---
+++ app/src/main/java/com/android/less1kotlin2206/MainActivity.java
@@ -0,0 +1,14 @@
+package com.android.less1kotlin2206;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
